14345a401cfb22e25200a2f1cd5a9103e02a4b9f
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-26 12:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var isDate = function (input) {
+	if((typeof input === 'object')||(/^\d{4}-\d{2}-\d{2}$/.test(input))==true){
+		return true;
+
+		return false;
+	}
+		
   //   write your code here
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+// const input = prompt("Enter Date.");
 alert(isDate(input));
