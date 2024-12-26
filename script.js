4b298fa06b159e102db5ea518eba7d8c7bcb6fe3
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-26 12:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var isDate = function (input) {
-	if((typeof input === 'object')||(/^\d{4}-\d{2}-\d{2}$/.test(input))==true){
-		return true;
 
-		return false;
-	}
+	return !isNaN(Date.parse(input));
+	
 		
   //   write your code here
 };
 
 // Do not change the code below.
-// const input = prompt("Enter Date.");
+const input = prompt("Enter Date.");
 alert(isDate(input));
